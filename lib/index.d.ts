ae944cad58b434f92873e7c97fa95de0a5872e79
kionell <sashakostyunin1@gmail.com>
2022-05-20 21:45:30 +0300

    Update build

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -43,61 +43,9 @@ declare class APICache extends Map<string, ICachedResponse> {
 }
 
 /**
- * Abstract URL generator.
- */
-declare abstract class URLGenerator {
-  /**
-     * Base link to the osu! website.
-     */
-  readonly SERVER_ROOT = 'https://osu.ppy.sh';
-  /**
-     * Base link to the osu! beatmap assets.
-     */
-  readonly ASSETS_ROOT = 'https://assets.ppy.sh';
-  /**
-     * Base link to the osu! avatars.
-     */
-  readonly AVATARS_ROOT = 'https://a.ppy.sh';
-  /**
-     * Generates a user profile URL by user ID or username.
-     * @param user User ID or username.
-     * @returns Generated user URL.
-     */
-  generateUserURL(user: string | number): string;
-  /**
-     * Generates a beatmap URL by beatmap ID.
-     * @param user Beatmap ID.
-     * @returns Generated beatmap URL.
-     */
-  generateBeatmapURL(beatmapId: string | number): string;
-  /**
-     * Generates a user avatar URL by user ID.
-     * @param user User ID.
-     * @returns Generated user avatar URL.
-     */
-  generateAvatarURL(userId: string | number): string;
-  /**
-     * Generates a beatmap cover URL by beatmapset ID.
-     * @param user Beatmapset ID.
-     * @returns Generated beatmap cover URL.
-     */
-  generateBeatmapCoverURL(beatmapsetId: string | number): string;
-  /**
-     * Generates a beatmap thumbnail URL by beatmapset ID.
-     * @param user Beatmapset ID.
-     * @returns Generated beatmap thumbnail URL.
-     */
-  generateBeatmapThumbnailURL(beatmapsetId: string | number): string;
-}
-
-/**
  * An API client.
  */
 declare abstract class APIClient {
-  /**
-     * URL generator.
-     */
-  abstract urlGenerator: URLGenerator;
   /**
      * An API cache.
      */
@@ -362,6 +310,54 @@ interface IHasUsers {
 }
 
 /**
+ * Abstract URL generator.
+ */
+declare abstract class URLGenerator {
+  /**
+     * Base link to the osu! website.
+     */
+  readonly SERVER_ROOT = 'https://osu.ppy.sh';
+  /**
+     * Base link to the osu! beatmap assets.
+     */
+  readonly ASSETS_ROOT = 'https://assets.ppy.sh';
+  /**
+     * Base link to the osu! avatars.
+     */
+  readonly AVATARS_ROOT = 'https://a.ppy.sh';
+  /**
+     * Generates a user profile URL by user ID or username.
+     * @param user User ID or username.
+     * @returns Generated user URL.
+     */
+  generateUserURL(user: string | number): string;
+  /**
+     * Generates a beatmap URL by beatmap ID.
+     * @param user Beatmap ID.
+     * @returns Generated beatmap URL.
+     */
+  generateBeatmapURL(beatmapId: string | number): string;
+  /**
+     * Generates a user avatar URL by user ID.
+     * @param user User ID.
+     * @returns Generated user avatar URL.
+     */
+  generateAvatarURL(userId: string | number): string;
+  /**
+     * Generates a beatmap cover URL by beatmapset ID.
+     * @param user Beatmapset ID.
+     * @returns Generated beatmap cover URL.
+     */
+  generateBeatmapCoverURL(beatmapsetId: string | number): string;
+  /**
+     * Generates a beatmap thumbnail URL by beatmapset ID.
+     * @param user Beatmapset ID.
+     * @returns Generated beatmap thumbnail URL.
+     */
+  generateBeatmapThumbnailURL(beatmapsetId: string | number): string;
+}
+
+/**
  * Abstract URL scanner.
  */
 declare abstract class URLScanner {
@@ -453,7 +449,7 @@ declare class APIFactory {
      * @param server Server name.
      * @returns API client.
      */
-  createAPIClient(server?: number): APIClient;
+  getAPIClient(server?: number): APIClient | APIClientWithOAuth;
   /**
      * Creates a new instance of URL scanner based on a server name.
      * @param server Server name.
@@ -698,7 +694,7 @@ declare class BanchoAPIClient extends APIClientWithOAuth implements IHasAttribut
   /**
      * Bancho URL generator.
      */
-  urlGenerator: BanchoURLGenerator;
+  readonly urlGenerator: BanchoURLGenerator;
   /**
      * Cached pairs of username & user ID.
      */
