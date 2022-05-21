c825a8e271a39e524bbfe64bf18096f28f0a0fd7
kionell <sashakostyunin1@gmail.com>
2022-05-22 01:27:05 +0300

    Add more data to API responses

--- src/Core/IAPIResponse.ts
+++ src/Core/IAPIResponse.ts
@@ -8,6 +8,16 @@ export interface IAPIResponse {
   url: string,
 
   /**
+   * Status code of this response.
+   */
+  status: number;
+
+  /**
+   * Error message of this reponse.
+   */
+  error: string | null;
+
+  /**
    * Data of this response.
    */
   data: any;
